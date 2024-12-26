d6ae87098ff74a5067a6d1ace56701085880b824
Chor WT <chorwaitong@gmail.com>
2024-12-26 16:33:50 +0800

    Add files via upload

---
+++ jarvis/utilities.py
@@ -0,0 +1,8 @@
+def warn_msg(warn_txt):
+    print ('[Warning] {}'.format(warn_txt))
+    
+def error_msg(msg_txt):
+    print ('[ERROR] {}'.format(msg_txt))
+    
+def congratz(msg_txt):
+    print ('[/] {}'.format(msg_txt))
